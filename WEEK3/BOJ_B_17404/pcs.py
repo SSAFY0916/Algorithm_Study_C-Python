0ca2fa77db8295c639b815e7fa9781e8d066f588
Cheolsoon <mn5724@naver.com>
2023-02-27 21:06:20 +0900

    [BOJ] 17404 RGB거리

--- WEEK3/BOJ_B_17404/pcs.py
+++ WEEK3/BOJ_B_17404/pcs.py
@@ -22,15 +22,15 @@ for i in range(n):
 ans = 1e9
 
 # dp[i][j] = 현재 j로 칠했을 때 최소 비용
-# ==> min(dp[i-1][j-1], dp[i-1][j+1]) + cost[i][j]
+# dp[i][j] = min(dp[i-1][j-1], dp[i-1][j+1]) + cost[i][j]
 for f in range(3):
     dp[0][f] = cost[0][f]  # 첫번째 집의 색깔
-    for i in range(1, n):  # 2번집 ~ n-1 번집 칠하기
-        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + cost[i][0]
-        dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + cost[i][1]
-        dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + cost[i][2]
+    for i in range(1, n):  # 2번집 ~ 마지막 집 칠하기
+        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + cost[i][0]  # 현재 집 R(빨간색) - 이전 집(초록 or 파랑)
+        dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + cost[i][1]  # 현재 집 G(초록색) - 이전 집(빨강 or 파랑)
+        dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + cost[i][2]  # 현재 집 B(파란색) - 이전 집(빨강 or 초록)
     for i in range(3):
-        if i == f:
+        if i == f:  # 마지막 집 색 != 첫번째 집 색
             continue
         ans = min(ans, dp[n-1][i])
     dp[0][f] = 1e9  # 첫번째 집 색깔 고정
